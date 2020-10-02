f7d9000e12c903e7aea934b6763ecf90447294f4
Ramiro Berrelleza <rberrelleza@gmail.com>
2020-10-01 22:33:01 -0700

    basic code
    Signed-off-by: Ramiro Berrelleza <rberrelleza@gmail.com>

---
+++ tutorial/sunset.py
@@ -0,0 +1,41 @@
+import os
+
+import cherrypy
+from cherrypy.process.plugins import Daemonizer, PIDFile
+import requests
+
+cur_dir = os.path.abspath(os.path.dirname(__file__))
+cert_path = os.path.join(cur_dir, "cert.pem")
+
+
+class Root:
+    @cherrypy.expose
+    def city(self, name):
+        r = requests.post("https://localhost:8444/", timeout=(2, 2), json={
+            "city": name
+        }, verify=cert_path)
+
+        if r.status_code != 200:
+            raise cherrypy.HTTPError(500, r.text)
+
+        cherrypy.response.headers["Content-Type"] = "text/plain"
+        print(r.json())
+        return "The sunset will occur at {} in {}".format(
+            r.json()["sunset"], name
+        )
+
+
+def run():
+
+    cherrypy.config.update({
+        "environment": "production",
+        "log.screen": True,
+        "server.socket_host": "0.0.0.0",
+        "server.socket_port": 8080,
+    })
+    PIDFile(cherrypy.engine, 'sunset.pid').subscribe()
+    cherrypy.quickstart(Root())
+
+
+if __name__ == '__main__':
+    run()
