637c28992a5b4ce1ab402d3c72aa07b08b2c2f53
Ramiro <ramiro@okteto.com>
2021-03-03 22:19:49 -0800

    make scripts path independent
    Signed-off-by: Ramiro <ramiro@okteto.com>

--- app/start.sh
+++ app/start.sh
@@ -1,1 +1,3 @@
-python astre.py & python sunset.py
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+python $DIR/astre.py & python $DIR/sunset.py
--- app/stop.sh
+++ app/stop.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 test -f /tmp/sunset.pid && pkill -F /tmp/sunset.pid
 test -f /tmp/astre.pid &&  pkill -F /tmp/astre.pid
 pkill -f astre.py
 pkill -f sunset.py
-cp valid-cert.pem cert.pem
+cp $DIR/valid-cert.pem $DIR/cert.pem
